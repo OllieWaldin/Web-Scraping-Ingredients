0418692fb73d7e3793ba4c8d4519c27e07bd1cdd
OllieWaldin <ollie@waldin.net~>
2023-08-19 14:57:57 -0700

    The start of checking information on soup

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("yay?")
+from bs4 import BeautifulSoup
+import requests
+url = "https://www.shcp.edu/academics/standardized-tests"
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup.title)
+else:
+    print("nope")

fdc32870dde8af5422f5635394b5821c9b6ab5f9
OllieWaldin <ollie@waldin.net~>
2023-08-26 14:35:15 -0700

    I DID ITgit status!

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ url = "https://www.shcp.edu/academics/standardized-tests"
 response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.title)
+    table = soup.find('div', attrs={"class": "container grid-lg contentblock-text"})
+    if table:
+        for date in table.find_all("li")[:4]:
+            print(date.get_text())
+    #print(table)
 else:
     print("nope")

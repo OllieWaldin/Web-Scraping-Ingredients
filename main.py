b07d9e628d4d700670a36888cac968ea5c42e8f7
OllieWaldin <ollie@waldin.net~>
2023-08-26 15:00:16 -0700

    cek

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://www.shcp.edu/academics/standardized-tests"
+url = "https://www.allrecipes.com/recipe/17481/simple-white-cake/"
 response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('div', attrs={"class": "container grid-lg contentblock-text"})
-    if table:
-        for date in table.find_all("li")[:4]:
-            print(date.get_text())
-    #print(table)
+    def getIngrs():
+        ingrs = []
+        table = soup.find('ul', attrs={"class": "mntl-structured-ingredients__list"})
+        if table:
+            for ingr in table.find_all("li"):
+                item = ingr.find("p")
+                if item:
+                    oneIngr = []
+                    for part in item.find_all("span"):
+                        oneIngr.append(part.get_text())
+                    ingrs.append(" ".join(oneIngr))
+        return ingrs
+                        
+    print(getIngrs())
 else:
     print("nope")
